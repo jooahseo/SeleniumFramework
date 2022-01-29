ff8fb45316bc4ea677082a32cbe3b1184fddbf70
Jooah Seo <seo.jooah@gmail.com>
2022-01-28 23:07:22 -0500

    add invalid login test

--- src/test/java/HomepageTest.java
+++ src/test/java/HomepageTest.java
@@ -1,5 +1,7 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pageObject.Homepage;
+import pageObject.LoginPage;
 
 import java.io.IOException;
 
@@ -12,6 +14,10 @@ public class HomepageTest extends Base{
 
         Homepage home = new Homepage(driver);
         home.clickLogin();
+
+        LoginPage login = new LoginPage(driver);
+        login.login("invalid", "password");
+        Assert.assertTrue(login.errorMessageExist("Invalid email or password."));
 
         Thread.sleep(3000);
         driver.quit();
