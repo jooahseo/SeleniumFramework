45c5bee0cca29cbee7d6b66e5b5454a1641d634d
Jooah Seo <seo.jooah@gmail.com>
2022-02-01 21:43:51 -0500

    Add class for extent reports

---
+++ src/main/java/ExtentReporter.java
@@ -0,0 +1,21 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporter {
+    static ExtentReports extent;
+
+    public static ExtentReports getReport(){
+        // ExtentReports , ExtentSparkReporter -> helps to generate a report
+        // user.dir == project path
+        String path = System.getProperty("user.dir")+"/reports/index.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Web Automation Results");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "Jooah Seo");
+
+        return extent;
+    }
+}
