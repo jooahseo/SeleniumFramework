459704820b9cc353ca1755561127198188285d0d
Jooah Seo <seo.jooah@gmail.com>
2022-02-01 21:02:56 -0500

    add getScreenshotPath method to save the screenshot

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,6 +1,10 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -10,7 +14,7 @@ public class Base {
     public WebDriver driver;
     public Properties prop;
 
-    public WebDriver initializeDriver() throws IOException {
+    public WebDriver getDriver() throws IOException {
         prop = new Properties();
         FileInputStream fis = new FileInputStream("/Users/jooahseo/Google Drive (seo.jooah@gmail.com)/Coding Bootcamp/Java/SeleniumFramework/src/main/resources/data.properties");
 
@@ -25,5 +29,13 @@ public class Base {
 
         }
         return driver;
+    }
+
+    public void getScreenshotPath(String testCaseName, WebDriver driver) throws IOException {
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        System.out.println("WHAT IS THE DIR "+ System.getProperty("user.dir"));
+        String file = System.getProperty("user.dir")+"/reports/"+ testCaseName +".png";
+        FileUtils.copyFile(source, new File(file));
     }
 }
